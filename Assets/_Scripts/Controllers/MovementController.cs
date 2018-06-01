83f9a1563e849db0cafc2b3a598ed93493240e6d
Thomas Brown <github@honourity.com>
2018-06-01 19:24:50 +1000

    added water dripping to player

--- Assets/_Scripts/Controllers/MovementController.cs
+++ Assets/_Scripts/Controllers/MovementController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2febd0e12dfe2cca9d404e82eed612dcd3c2edf11c79139ccef6a370fa31944
-size 1283
+oid sha256:770de6d865a60d0d35f51cbc133110c98f9c76581e7af0db0d3b9e886a38841a
+size 1282
---
+++ Assets/_Scripts/Controllers/Water/PuddleController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5de978b412c7d49ac01001f103b03afbf5bef3a69e9ad50c9c313445cf556152
+size 4088
