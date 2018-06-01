0f540a53111d6b676a6ec54b06948a03de4637d6
Thomas Brown <github@honourity.com>
2018-06-01 15:54:05 +1000

    fixed sprite layer nno-dynamic and added collider thing

--- Assets/_Scripts/Controllers/SpriteLayerController.cs
+++ Assets/_Scripts/Controllers/SpriteLayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:264c45d2d5f4592cb7c31af5d32af5a5b711d3f3b21c80339c26aaca2c468342
-size 703
+oid sha256:7113510257e90c42c430edf714c9706d086d9c72c9f83d45aa1e664ac826ff13
+size 804
