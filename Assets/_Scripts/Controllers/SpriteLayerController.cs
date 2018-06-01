1fd6eb0d7c51710d94b67f117c65527b297b3c7f
Thomas Brown <github@honourity.com>
2018-06-01 14:14:31 +1000

    added collider test check on spritelayers controller

--- Assets/_Scripts/Controllers/SpriteLayerController.cs
+++ Assets/_Scripts/Controllers/SpriteLayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cfc29a35851f71993c0057e674779c79776dd82c8f7b970210d7141fb4c8e628
-size 571
+oid sha256:264c45d2d5f4592cb7c31af5d32af5a5b711d3f3b21c80339c26aaca2c468342
+size 703
