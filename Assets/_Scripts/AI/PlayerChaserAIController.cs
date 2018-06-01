9fd47fb7d454dfe7ff30b4e8e15d2d2bf801eaf2
arvz <arvin.g@gmail.com>
2018-06-01 23:27:18 +1000

    Tweaks to some prefabs. Debug options added to PlayerChaser

--- Assets/_Scripts/AI/PlayerChaserAIController.cs
+++ Assets/_Scripts/AI/PlayerChaserAIController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fed7dd09ba3cc4db2d5120997bb8e672c6be51c5c9e996b2d51f5abf10efb558
-size 1824
+oid sha256:f2f0c9219d1eff583a0e306f92a3d0434c32bab4c038ba17472fd2f217bc7e0d
+size 2041
--- Assets/_Scripts/Controllers/MovementController.cs
+++ Assets/_Scripts/Controllers/MovementController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:770de6d865a60d0d35f51cbc133110c98f9c76581e7af0db0d3b9e886a38841a
-size 1282
+oid sha256:a49f68fe1e737765d6cb14c28853ef3f261e9b3bbcbbe53c649d0423a2f18781
+size 1198
