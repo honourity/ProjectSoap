6213edfc57258012386394232b8cb4087e152187
Thomas Brown <github@honourity.com>
2018-06-04 15:03:28 +1000

    removed layer controller running in edit mode, reverted some scene stuff to original

--- Assets/_Scripts/Controllers/SpriteLayerController.cs
+++ Assets/_Scripts/Controllers/SpriteLayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33a93460118146eacd4597159db07f8ba6bad1fee75a5c24425d5ace6417b86c
-size 838
+oid sha256:1b82798d2e25dcd9d89831079a54eab534020db0abf55f677e2eaa47726a02d7
+size 818
