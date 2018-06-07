875c5bcb34be420bbb53e16f232133f60a8d6788
Thomas Brown <github@honourity.com>
2018-06-08 00:16:56 +1000

    re-added locking, proven to cause VERY minimal performance detriment

--- Assets/_Scripts/Utilities/Messaging.cs
+++ Assets/_Scripts/Utilities/Messaging.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6a427fc35057a5718a9a410c643467643abd2eb01957d91ecac13eecf89ccec
-size 4598
+oid sha256:05020ce3472040831aef18290d7da7e734853f7f304e19ce0f1fd3fca1785294
+size 4566
