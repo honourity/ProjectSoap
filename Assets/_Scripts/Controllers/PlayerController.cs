41dd20850de9c0a0eb675ad372ed1194435131b0
Thomas Brown <github@honourity.com>
2018-06-13 22:19:31 +1000

    made tileset walls reflect (hacky)

--- Assets/_Scripts/Controllers/PlayerController.cs
+++ Assets/_Scripts/Controllers/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d2befcc1f88daf42607d4308c3228f22f52c787000e2bfbd19da9027bc9e97c
-size 2044
+oid sha256:56f7e1f1f1392379392fa6c4c9cd19fe02982f1bede332a2c3bb146d4f474aa6
+size 1969
--- Assets/_Scripts/Controllers/Water/ReflectionController.cs
+++ Assets/_Scripts/Controllers/Water/ReflectionController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36a61e4e8550b691964d744650a6c6546f73a3a09ce1ab0cc5ade60da18d1efd
-size 1759
+oid sha256:69c54c757e58dac74ed395dd88802af960d7e0c3a5dba9d6fa2851dd428ed9c9
+size 3371
