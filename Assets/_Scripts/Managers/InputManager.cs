11459333b0d54cb743948b7b7d5fdc702571a5c6
arvz <arvin.g@gmail.com>
2018-06-13 17:11:42 +1000

    Arena: Seperated top wall into its own Tilemap layer. Player: Made mouse button work to attack.

--- Assets/_Scripts/Managers/InputManager.cs
+++ Assets/_Scripts/Managers/InputManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:692dbf49811dca20ee355b568df48b8612666993b9e219d3c54c318895a6c8af
-size 1991
+oid sha256:5dc3ec2dc2a510a5dd49fb2ebee00a41ea5817f6a6c3cd50131198c13e14954e
+size 2027
