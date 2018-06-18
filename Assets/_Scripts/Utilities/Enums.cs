7f9145db548d66e0c21b0a3f6a9589b16d9b8be0
Thomas Brown <github@honourity.com>
2018-06-18 15:25:17 +1000

    removed some unused movetypes

--- Assets/_Scripts/Utilities/Enums.cs
+++ Assets/_Scripts/Utilities/Enums.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b66d8965198f14d8c5dd111e0a51393aa7654fb95304d3f6bf304e6558d15289
-size 612
+oid sha256:9f0cdb82da7124e57a75ec839fc3566b192c05be4b6e465336e478f150f71d3d
+size 596
