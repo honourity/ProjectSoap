87a75ba49da87d1b65975c574f33884033eff9f3
arvz <arvin.g@gmail.com>
2018-06-19 15:07:12 +1000

    Shower: Shower now respawns soaps.

--- Assets/_Scripts/Controllers/SoapSpawningController.cs
+++ Assets/_Scripts/Controllers/SoapSpawningController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52469e15625072404320b5cfbcd7c075851d35a16a01e5a44eaccefd30daea59
-size 270
+oid sha256:eed8a7e91bb559c40495022730febf477811eb01543354a006a9b1483a11352c
+size 1278
--- Assets/_Scripts/Utilities/Enums.cs
+++ Assets/_Scripts/Utilities/Enums.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4514d269d39dc3fcaf15b355c7c370c2cd2227234cb6f9bda7f8aef59955ca03
-size 661
+oid sha256:551610755c79d9463cda87663b514b890ffc800116690901aec40729494592fa
+size 712
