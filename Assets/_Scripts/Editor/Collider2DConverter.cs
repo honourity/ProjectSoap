5b6191423b9479e2d3bcd07ce90a9bbd7000e5f0
arvz <arvin.g@gmail.com>
2018-06-26 17:40:02 +1000

    Removed some unused variables

--- Assets/_Scripts/Editor/Collider2DConverter.cs
+++ Assets/_Scripts/Editor/Collider2DConverter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81ac5af8cb029a91bfaff49140036190e4d2f55c97850eebc9586e6fca6fdf9c
-size 1919
+oid sha256:3f8fd8cff1713dbd095b3766215dc6f29ac60616f3d503f236454eaf16d7c367
+size 1906
