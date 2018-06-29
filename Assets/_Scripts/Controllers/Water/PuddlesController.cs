d22f46dde8021292f95cf8094f7da87a85543bac
Thomas Brown <github@honourity.com>
2018-06-29 16:18:33 +1000

    water system - performance increase, and fixed higher accuracy collider generating, made blob detection threaded

--- Assets/_Scripts/Controllers/Water/PuddlesController.cs
+++ Assets/_Scripts/Controllers/Water/PuddlesController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79108a5cb32b90a6544ea4aefb0a18d5cb47ff5091eac444940cc5b42d331d83
-size 10511
+oid sha256:58e6f7b4b2837218c784061851cdc53c96ba318baff02576a684a0f0a4e8f13a
+size 11577
