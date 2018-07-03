b867f019691a837ce65cd4ac07c8ba079c03fca7
arvz <arvin.g@gmail.com>
2018-07-03 11:59:36 +1000

    PlayerController changes

--- Assets/_Scripts/Controllers/PlayerController.cs
+++ Assets/_Scripts/Controllers/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6062e3f2897edae1ea5cf873018cca53e5b4f5cea17f56ffe7612a58ad2d35a
-size 5794
+oid sha256:446383cac381275af636c9b2fa8e86028273eaa9aef789ebc64f14797b8127d1
+size 6081
