aa4d86948c98b0789c1bcb746b9135de056581a1
Thomas Brown <github@honourity.com>
2018-07-04 13:26:01 +1000

    added alpha for reflections

--- Assets/_Scripts/Controllers/Water/ReflectionController.cs
+++ Assets/_Scripts/Controllers/Water/ReflectionController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f368103fb4fe2270c8b31d8726425066efd9e015b4c7e32542d015905c983ca6
-size 3408
+oid sha256:f87d0ae781a05476f0ecb5b4b0538ea20ddd1132fc79dce1bd9d9688d5e9d805
+size 3680
