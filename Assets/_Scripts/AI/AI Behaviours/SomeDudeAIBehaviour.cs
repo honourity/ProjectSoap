5ee44e43463d2ec43be95c42ab210e088c3bac9f
arvz <arvin.g@gmail.com>
2018-07-04 16:14:53 +1000

    AIBehaviour: Hidden AIModule components by default. Added Editor only toggle to show the components.

--- Assets/_Scripts/AI/AI Behaviours/SomeDudeAIBehaviour.cs
+++ Assets/_Scripts/AI/AI Behaviours/SomeDudeAIBehaviour.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:690c990b4b5bde48f48b1d298f7da30d634b090ff26aac21e53bb249ec2b130e
-size 2736
+oid sha256:78ded573aa6a61527159f394360500db717d179055df1a7cf92aa7861f4622c6
+size 2694
