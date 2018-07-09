770e86bb4209822a454cf3edb57237d85ac333bc
arvz <arvin.g@gmail.com>
2018-07-09 15:08:25 +1000

    SomeDudeAI: Added random chance of this AI continuing to wander around after being cleaned

--- Assets/_Scripts/AI/AI Behaviours/SomeDudeAIBehaviour.cs
+++ Assets/_Scripts/AI/AI Behaviours/SomeDudeAIBehaviour.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6878c3324d653528c2a31018a3272e2bd859f00a365bd90244aadff51ae7a685
-size 3194
+oid sha256:db66fe7c46feae52c3f66370edc4af3908cd6690e513e6a0abc139b3c5468fad
+size 3591
