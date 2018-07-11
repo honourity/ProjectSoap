38e22412cdba9f399416cba9deb1d6378c9af683
Thomas Brown <github@honourity.com>
2018-07-11 14:25:14 +1000

    #65 - added CanMove to MovementController which forced movetype NONE, and refactored how special moves enable/disable movementcontroller

--- Assets/_Scripts/Controllers/Soap/SoapThrowController.cs
+++ Assets/_Scripts/Controllers/Soap/SoapThrowController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1713f7f69eb55f6ec3ea43a203b42790f5ff169af2797c78028e29a12e788045
-size 4438
+oid sha256:d24d4b6df8183ee9d3033ab29c964acf99d81bd5a4fdbd8d08e7fdd41f54c9b4
+size 4453
