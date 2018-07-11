f3c1eaad65acdb301772633a7ab173505a7853a7
arvz <arvin.g@gmail.com>
2018-07-11 11:17:40 +1000

    Upgraded Project to 2018.2

--- Assets/External Assets/2d-extras/Brushes/GameObject Brush/Scripts/Editor/GameObjectBrush.cs
+++ Assets/External Assets/2d-extras/Brushes/GameObject Brush/Scripts/Editor/GameObjectBrush.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:283031ef3ef497f4e4c048fedcf87b91cdb32c74854fd4bf90368747c0f00985
-size 14890
+oid sha256:e2db7601138cce9e89a290a70b0ef2c1e44fd028c0871facc79b1fc324e02fea
+size 14907
