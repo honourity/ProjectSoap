91c8022dcdc2d5edaf8dff62b70731d2fe9553d5
Thomas Brown <github@honourity.com>
2018-07-12 16:14:17 +1000

    removed unused variable

--- Assets/_Scripts/Controllers/Player/PlayerController.cs
+++ Assets/_Scripts/Controllers/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1c8ceabac7466471682ca050528769d7facca94bb56c35b2c9d7411cd05db07
-size 6388
+oid sha256:618782bb1f9f72dae496d0ce13af1cd65f308aef4efd3aab83a18e60e1cb0028
+size 6307
