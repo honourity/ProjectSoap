5fb1698eaacdd014993851fc370abae984dbf7a5
Thomas Brown <github@honourity.com>
2018-07-12 13:39:40 +1000

    added null checks in some AI stuff and fixed exceptions

--- Assets/_Scripts/AI/AI Behaviours/SomeDudeAIBehaviour.cs
+++ Assets/_Scripts/AI/AI Behaviours/SomeDudeAIBehaviour.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2144f6f7d90071743e487071a1e3811a6bbd39aaf39afcf309d087ee97ad3d19
-size 3490
+oid sha256:f583b698fd8c40eae752bb3d7284f3a9d1b9e330ab55596492cc05e4e279a84a
+size 3491
--- Assets/_Scripts/AI/AI Modules/PlayerChaserAIModule.cs
+++ Assets/_Scripts/AI/AI Modules/PlayerChaserAIModule.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edf81acf825507628793bc2f06edaa20cdb3f67129d2ea68b590d03e2abd06aa
-size 1903
+oid sha256:d84d79bedbe2fdb67efe20fb9209051b28e9a76313933c960902fa3106285edd
+size 1868
