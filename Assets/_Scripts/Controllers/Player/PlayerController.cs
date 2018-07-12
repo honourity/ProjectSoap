de75b22de4130320b5696e3de3d1df6376283793
Thomas Brown <github@honourity.com>
2018-07-12 19:10:17 +1000

    added mix of trigger/collider hygiene drain

--- Assets/_Scripts/Controllers/Player/PlayerController.cs
+++ Assets/_Scripts/Controllers/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3e06955302582337df775b8cee089acc4c7ea66b8367b84ce36e53c22b7db48
-size 6246
+oid sha256:edf63744b40972d0bd2fc26731acfaf8d6b26e82df0dde781687d51372dc966f
+size 6357
