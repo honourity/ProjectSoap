d2304061a9dc7b0c4bc7da0be2285eb5e38a319d
Thomas Brown <github@honourity.com>
2018-07-12 18:57:08 +1000

    got grabbing/being grabbed, falling down and grab cooldown working

---
+++ Assets/_Scripts/Controllers/GrabbingController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6c5224f2c28020b78ae5244060396da367f887373828db1df67b7fb4e0e5d217
+size 3542
--- Assets/_Scripts/Controllers/Player/PlayerController.cs
+++ Assets/_Scripts/Controllers/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38cb379c342f8a6671338b63d4e6ae36454f77428e5680ab9b4612f58aa1867c
-size 6314
+oid sha256:b3e06955302582337df775b8cee089acc4c7ea66b8367b84ce36e53c22b7db48
+size 6246
