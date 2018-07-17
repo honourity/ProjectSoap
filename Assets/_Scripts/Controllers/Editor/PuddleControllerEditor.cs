4e57be4c065e202759a68661e07fcbf0c320d7f4
arvz <arvin.g@gmail.com>
2018-07-17 18:47:51 +1000

    Puddles: No longer sets the shader in code, instead uses a material with the shader on it. PuddleControllers execute in edit mode.

--- Assets/_Scripts/Controllers/Editor/PuddleControllerEditor.cs
+++ Assets/_Scripts/Controllers/Editor/PuddleControllerEditor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff4b3e62567ba811d924dbcfe3b587d3fbb24c2b87ffa231d2bef28f0e245fd1
-size 824
+oid sha256:59d4849d79af8cf7f173b64a407887423938764e48e9ff89c155222ff4bcd7b4
+size 781
--- Assets/_Scripts/Controllers/Water/PuddleController.cs
+++ Assets/_Scripts/Controllers/Water/PuddleController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61c034c6dc6541d8d70ef04b552738974ad19d2c66281eafeb9054cd2bda2158
-size 6753
+oid sha256:50bcf1dd3ac857fee7d05248ce7cfe7370cacea72ebe285aaaf055a38a303d8a
+size 6774
