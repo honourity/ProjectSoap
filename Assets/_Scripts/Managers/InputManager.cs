35afd7f7a4e237ccda1a1968e85f18b2cd7007ad
Thomas Brown <github@honourity.com>
2018-07-17 14:51:05 +1000

    completed #75 - changing controls

--- Assets/_Scripts/Managers/InputManager.cs
+++ Assets/_Scripts/Managers/InputManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82f09ec38edb1ad1a5f3c49de084944b8f391895e7e2d31f350de3ce0b2b64fb
-size 1810
+oid sha256:cf2eb9fd9335eff0f13bfcc5aee3aaca9c42e7309ff538b41f1fa972249a73ff
+size 2053
