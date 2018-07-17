41983ba365a9c208f5677543f7262361af22a1fd
Thomas Brown <github@honourity.com>
2018-07-17 16:18:05 +1000

    fixed missing player idle east animations, fixed hand jitter (by disabling pixelperfect camera script)

--- Assets/_Scripts/Controllers/GrabbingController.cs
+++ Assets/_Scripts/Controllers/GrabbingController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2aac80a47b1de2145d669547a3565ec69c5bfaa16e952e2bed1d39a18a715b8b
-size 4100
+oid sha256:70fdee886a7c658f194ced4e1285d603da72e9cff3e2deb057fc75749f4097f8
+size 4101
