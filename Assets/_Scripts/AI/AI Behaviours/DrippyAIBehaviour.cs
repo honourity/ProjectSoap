af401cf387cc0db5306aae774c052886807a0e51
arvz <arvin.g@gmail.com>
2018-07-18 23:04:56 +1000

    Puddles: Set layer order to -500 Background to make sure it's behind everything. Soap Arrows: Started

--- Assets/_Scripts/AI/AI Behaviours/DrippyAIBehaviour.cs
+++ Assets/_Scripts/AI/AI Behaviours/DrippyAIBehaviour.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43dfb8c92fda4098404d048dc1ad258eb022edc34216f440ca16130d96c5bb32
-size 3865
+oid sha256:aeafe964d76b6b4a3339f03d9e08a253ed11a63c36e145dce4ae3543a94b6483
+size 3944
---
+++ Assets/_Scripts/Controllers/UI/SoapArrowsController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4c5cf80fd70fd46a18ad40db036a43ef62ed59e65a2fbe1767d5954d9b0280d2
+size 438
