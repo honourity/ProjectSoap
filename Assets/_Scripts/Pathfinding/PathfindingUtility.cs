0d8012a6019a180e021c5ad176286bb01f0c8387
arvz <arvin.g@gmail.com>
2018-07-18 16:15:15 +1000

    Removed debug log

--- Assets/_Scripts/Pathfinding/PathfindingUtility.cs
+++ Assets/_Scripts/Pathfinding/PathfindingUtility.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de7ed70bdc6f747a402695262315ca1e4a3b2d36663eaa4690ba1770637fc3ae
-size 3249
+oid sha256:9609cbd45d37924e3cdc2e788ae8c2738e35baff697ddd36c098645a38764206
+size 3205
