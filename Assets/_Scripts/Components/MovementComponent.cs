777812cb401b969289f7fde73c35614f760fb12b
Thomas Brown <github@honourity.com>
2018-07-19 13:44:17 +1000

    added MovementComponent as backing data for MovementController, so player and npcs can have their own individual movespeeds

---
+++ Assets/_Scripts/Components/MovementComponent.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8bb57a58060f157d40334110912bb1d451ddc8d02198740f3f5384fd144bec28
+size 176
--- Assets/_Scripts/Controllers/Player/PlayerController.cs
+++ Assets/_Scripts/Controllers/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05b30454537cb306accedaa0ec6919486a361a54cee25b17c2ee833c2dbd12a5
-size 6995
+oid sha256:35c56ee76489ca33012611bef7407143b8e92f0a3026e1fefab47db9a0686e97
+size 7275
