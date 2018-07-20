b06a53f984b1bd8b63d6d83b6366f2a3f4921668
Thomas Brown <github@honourity.com>
2018-07-20 20:01:44 +1000

    removed some soap from scene, disabled puddle slipperyness

--- Assets/_Scripts/Controllers/Water/PuddleController.cs
+++ Assets/_Scripts/Controllers/Water/PuddleController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:969200ee03961a64383c17c9395286de9a2473f80aff5489738228f055d4706f
-size 6783
+oid sha256:bae8d32bf68a61f10bbddbaf1cd25aae1080a763926ed98e4fd4724ebefc48c7
+size 6924
