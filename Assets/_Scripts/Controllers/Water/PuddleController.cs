2b958d6cf3ef9a459422df57863f171e7b669f57
Thomas Brown <github@honourity.com>
2018-07-20 22:27:23 +1000

    #111 - done, added player absorbing dirtyness from puddles

--- Assets/_Scripts/Controllers/Water/PuddleController.cs
+++ Assets/_Scripts/Controllers/Water/PuddleController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b90cdde13c68c41a0b21a4281d7736b5d4fb6dd9d3329daa258317914dd03448
-size 7129
+oid sha256:0bc27c63322692880222d1a0f37ceee844868f8f30a16169c915055fda926a0a
+size 6824
