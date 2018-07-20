71aa02bd6c030a705a7bea76a8c5eef5f8fbb436
Thomas Brown <github@honourity.com>
2018-07-20 22:12:23 +1000

    fixed sprite reflections not animating

--- Assets/_Scripts/Controllers/Water/ReflectionController.cs
+++ Assets/_Scripts/Controllers/Water/ReflectionController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f87d0ae781a05476f0ecb5b4b0538ea20ddd1132fc79dce1bd9d9688d5e9d805
-size 3680
+oid sha256:e5eeb326032eb30148ffd155ecf5d21198ee4aa6c50f3d5141109e03aa7b1318
+size 3673
