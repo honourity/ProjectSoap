ba99ea0e4100c45591a2deb91ddcdfaa11b99c35
arvz <arvin.g@gmail.com>
2018-07-20 22:00:44 +1000

    Soap: Hygiene value increased to 100. Zombie AI: Re-implemented. Drippy: Starting Hygiene to -50

--- Assets/_Scripts/AI/AI Behaviours/JanitorAIBehaviour.cs
+++ Assets/_Scripts/AI/AI Behaviours/JanitorAIBehaviour.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20f9ae12caf96e098b387dd808a87f9e89cdf6a6e857cffe69c3f9a4a0a243a0
-size 2286
+oid sha256:eb619690c0c46f953eccfb507ca95966fffc1c0ac7de8eb0ab5fdc0c1ef39c08
+size 2288
