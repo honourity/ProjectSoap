00ee65ee43e217efccdf907c80f376102c672f6f
Thomas Brown <github@honourity.com>
2018-07-24 16:23:51 +1000

    removed some debug code and commented out code

--- Assets/_Scripts/Controllers/MovementController.cs
+++ Assets/_Scripts/Controllers/MovementController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cad1a56a6abc4b52aae02d982d8a4b3fd0c9bce6ad2f36632be69df731b87774
-size 7392
+oid sha256:d6a2554f9e904305d9c075b15c44220a82dfd3067e16ae8de87a2a0d249928fa
+size 6831
