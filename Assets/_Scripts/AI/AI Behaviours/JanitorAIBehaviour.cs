a23093f02aee3f292e9fb09636e06b0180fd55b6
Thomas Brown <github@honourity.com>
2018-07-24 18:52:36 +1000

    tiny tweaks to janitor

--- Assets/_Scripts/AI/AI Behaviours/JanitorAIBehaviour.cs
+++ Assets/_Scripts/AI/AI Behaviours/JanitorAIBehaviour.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95aa7eea586b901c6aec38a5df16e7b876288847b2f1f1dc3e5f3e53ab5fd3c0
-size 2537
+oid sha256:4150001edac31e527c4d8c75b17ea84f22dd30abbf63fd72f2119b18cbb95bd1
+size 1978
