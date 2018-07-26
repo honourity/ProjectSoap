b5fe5a0fcda725fd4c8f0bf6bfd1174958164229
arvz <arvin.g@gmail.com>
2018-07-26 18:53:07 +1000

    Start Screen: New Start Screen with the controls layout info. Arena: Baths now part of tilemaps. Drippy AI: Seeks closest node with Bath tag instead of searching for WaterSources. Sets traversable tags in DrippyAIBehaviour.

--- Assets/_Scripts/AI/AI Modules/GetWaterAIModule.cs
+++ Assets/_Scripts/AI/AI Modules/GetWaterAIModule.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64fb1cb651be42c7c3dd0cde382b41d90723865c93c5bc5cd64b537bb6b2b452
-size 2182
+oid sha256:302502be7ae51bb2f20d8bbd4fdb611214dc31a55af0487fd478c312a61cdaac
+size 2123
---
+++ Assets/_Scripts/Controllers/UI/VersionTextUIController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:22e6c93bb808e5570bd707e635c4a9b5a0975f6465e0bb3247e5880b65061297
+size 462
