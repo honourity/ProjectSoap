8cacf99679d41936c751fa4753c50eebea37581a
arvz <arvin.g@gmail.com>
2018-07-29 01:18:43 +1000

    ExitAIModule/AIs: Fixed AIs not exiting the arena properly. PlayerMaskInteractionController: Moved Mask interaction (for baths) to its own script to be more modular and easier to remove/replace later.

---
+++ Assets/PlayerMaskInteractionController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:147f7cac37ba09bcc88ea8e35bf7ebd93460698f3f497a9f965710511641f283
+size 858
--- Assets/_Scripts/Controllers/Player/PlayerController.cs
+++ Assets/_Scripts/Controllers/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5aa86e860d7dc2eca0d0c661c55a92baf33bbec0de9ddf6a1f92236b5ccc373c
-size 8183
+oid sha256:9d3acc71f4f824df3b7e84dbf4980984dce41ab007fce2ef781035ec16204e2e
+size 7929
