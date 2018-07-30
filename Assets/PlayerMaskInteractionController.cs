08b9204f9dc6c0e687f2ee4a38698cabf945aa49
arvz <arvin.g@gmail.com>
2018-07-30 15:29:28 +1000

    Unity Version: Upgraded to 2018.2.1f1 PlayerMaskInteractionController: Fixed player sprites not showing at all when doing a dash in baths. Soap Prefab: Reset hygiene back to 10.

--- Assets/PlayerMaskInteractionController.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3add6492f3f8a2293b7800eda950e7a4cd0f87f7e92ef44d9444dd38f8ab9659
-size 908
---
+++ Assets/_Scripts/Controllers/Player/PlayerMaskInteractionController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b071f406d8de279fe2d280659f58892ecada063e842e793889b5f636bcd8d543
+size 1213
