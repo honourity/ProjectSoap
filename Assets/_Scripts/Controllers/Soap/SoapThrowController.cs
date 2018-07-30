407f9269a3919efb685176bb9267845539b6d986
Thomas Brown <github@honourity.com>
2018-07-31 03:17:09 +1000

    fixed sprite layer issue with soap shadow and soap ghost when throwing north, fixed bounce direction on diagonals

--- Assets/_Scripts/Controllers/Soap/SoapThrowController.cs
+++ Assets/_Scripts/Controllers/Soap/SoapThrowController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f1e297f130d6cf15ea18f6f420134a8d71093aa06872f2edb65983a627404ca
-size 6375
+oid sha256:496672e995e37a4cd4584642edac7228a4df5b9981fc2bf9d021725a7fe764b9
+size 6434
