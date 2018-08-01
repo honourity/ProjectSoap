5bb1d4555564f7de20dae4d6f860d60a7501771b
Thomas Brown <github@honourity.com>
2018-08-02 02:55:59 +1000

    better alignement of grabbing sprites

--- Assets/_Scripts/Controllers/GrabbingController.cs
+++ Assets/_Scripts/Controllers/GrabbingController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc187f284ddd8b5242bd33bc715983a64e5cfb2b1c6f3ef7bf59a097fb115c5b
-size 5348
+oid sha256:0b4a6d2d294da274571642444aa55b8360dd667a0f08b1e0d0c8f6eb2214a333
+size 5725
--- Assets/_Scripts/Controllers/Soap/SoapThrowController.cs
+++ Assets/_Scripts/Controllers/Soap/SoapThrowController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:496672e995e37a4cd4584642edac7228a4df5b9981fc2bf9d021725a7fe764b9
-size 6434
+oid sha256:92d3cf65a784925d03f777a0ff14fad4e072b2f04fd2b9daaea10cd413651dba
+size 6433
