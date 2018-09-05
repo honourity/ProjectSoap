25cf724d3f9d62aecd714b3897b4ea9865f1ad45
Thomas Brown <github@honourity.com>
2018-09-05 13:13:05 +1000

    added npc knockback on stun (and made player stun while sliding over water)

--- Assets/_Scripts/Controllers/MovementController.cs
+++ Assets/_Scripts/Controllers/MovementController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e217c0f47ea88ec9036a66302c784d8826deebdd1739c8b7ea6fdcdb55b2cbe
-size 6808
+oid sha256:ffbc3efbd73edaa47fd02f773fe5afb4122f4539e9d4af8f89356498f1e25d0c
+size 7379
--- Assets/_Scripts/Controllers/Player/PlayerController.cs
+++ Assets/_Scripts/Controllers/Player/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:809ce47c80c4563d2bc265c39ac93c972435e5f99ff5dc1a3a38ce4b44d2f776
-size 8373
+oid sha256:7f0e9950af1126c2e0b779e22f983a58df93dacf9e16988eeada3a1349fc29bc
+size 8140
